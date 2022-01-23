9e8f9ed7f0419ccc6c8af1320ebc9160994f5ce2
Koaxy <92256387+Koaxy@users.noreply.github.com>
2022-01-23 21:56:15 +0000

    Uploaded the code
    This currently supports INT and Strings, more coming soon ™️

--- QueueAPI.cpp
+++ QueueAPI.cpp
@@ -1,1 +1,46 @@
+#include "QueueAPI.h"
 
+KoaxyClass::~KoaxyClass()
+{
+}
+
+KoaxyClass::KoaxyClass()
+{
+}
+
+int KoaxyClass::pushIntToQueue(int Value)
+{
+    this->IntQueue.push(Value);
+	Value = this->IntQueue.front();
+    this->IntQueue.pop(); // pop it so no conflict
+    return Value;
+}
+
+int KoaxyClass::pushIntToQueuePriority(int Value)
+{
+    this->IntPriorityQueue.push(Value);
+    this->IntPriorityQueue.pop();
+    return Value;
+}
+
+std::string KoaxyClass::PushStringToQueue(std::string str)
+{
+    this->StrQueue.push(str);
+	str = this->StrQueue.front();
+    this->StrQueue.pop();
+	return str;
+}
+
+std::string KoaxyClass::PushStringToPriorityQueue(std::string str)
+{
+    this->StrPriorityQueue.push(str);
+	int size = this->StrPriorityQueue.size();
+	printf("the sizeof the string is: %i\nstr is (Priority Queue) %s \nsizeof queue is: %i\n", str.size(), str.c_str(), size);
+	this->StrPriorityQueue.pop();
+	return str;
+}
+
+void KoaxyClass::ClearStringQueue()
+{
+	this->StrPriorityQueue.pop();
+}
