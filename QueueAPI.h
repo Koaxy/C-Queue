291b685ef3fb908678fe07c42c358422d9847c1c
Koaxy <92256387+Koaxy@users.noreply.github.com>
2022-01-23 21:56:52 +0000

    Yummy :tm:

--- QueueAPI.h
+++ QueueAPI.h
@@ -1,1 +1,27 @@
 
+#pragma once
+#include <Windows.h>
+#include <memoryapi.h>
+#include <iostream>
+#include <vector>
+#include <string>
+#include <fstream>
+
+#include <queue>
+
+class KoaxyClass
+{
+public:
+	~KoaxyClass();
+	KoaxyClass();
+	int pushIntToQueue(int Value);
+	int pushIntToQueuePriority(int Value);
+	std::string PushStringToQueue(std::string str);
+	std::string PushStringToPriorityQueue(std::string str);
+	void ClearStringQueue();
+private:
+	std::queue<std::string> StrQueue;
+	std::priority_queue<std::string> StrPriorityQueue;
+	std::queue<int> IntQueue;
+	std::priority_queue<int> IntPriorityQueue;
+};
