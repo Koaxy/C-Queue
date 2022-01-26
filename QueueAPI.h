0a0e8dda61ef61d378095a12a6290c2f872c2c29
Koaxy <92256387+Koaxy@users.noreply.github.com>
2022-01-26 21:49:56 +0000

    Added boolean value queues

--- QueueAPI.h
+++ QueueAPI.h
@@ -18,10 +18,14 @@ public:
 	int pushIntToQueuePriority(int Value);
 	std::string PushStringToQueue(std::string str);
 	std::string PushStringToPriorityQueue(std::string str);
-	
+	bool PushBoolToQueue(bool Bo);
+	bool PriorityBoolQueue_Impl(bool Bo);
+	void ClearStringQueue();
 private:
-	std::queue<std::string> StrQueue;
-	std::priority_queue<std::string> StrPriorityQueue;
-	std::queue<int> IntQueue;
-	std::priority_queue<int> IntPriorityQueue;
+	std::queue <bool> boolQueue;
+	std::priority_queue<bool> PriorityBoolQueue; // Added Boolean value queues
+	std::queue <std::string> StrQueue;
+	std::priority_queue <std::string> StrPriorityQueue;
+	std::queue <int> IntQueue;
+	std::priority_queue <int> IntPriorityQueue;
 };
